e58feebb14af177632bb130b32690cbca7c9f60d
Dustin Flanary <singflan@gmail.com>
2016-06-22 17:49:40 -0600

    initial commit

---
+++ app/src/androidTest/java/com/tryazon/tryazon_info_page/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.tryazon.tryazon_info_page;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
